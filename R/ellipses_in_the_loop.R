3dacc32dacf349cbb03b85baedf3fa8053af0261
ksawicka <kasia.sawicka@wur.nl>
2016-04-14 17:52:05 +0200

    Dummy to run ellipses in the loop.

---
+++ R/ellipses_in_the_loop.R
@@ -0,0 +1,18 @@
+
+mymodel <- function(a, b) {
+  a*b
+}
+
+run <- function(model = mymodel, n, ...) {
+  
+  myrun <- numeric(n) # n cannot be higher than number of simulations of uncert var.
+  for (i in 1:n) {
+    myrun[i] <- model(...)
+  }
+  myrun
+  
+}
+
+a <- sample(1:10, 6, replace = TRUE)
+b <- 2
+c <- run(model = mymodel, n = 6, a = a , b = b)
