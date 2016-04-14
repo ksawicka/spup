b343254bb5cbc2e3c7a0079fcfe4e21089da0727
ksawicka <kasia.sawicka@wur.nl>
2016-04-14 15:55:31 +0200

    Added roxygen sceleton.

--- R/defjointnonspatial.R
+++ R/defjointnonspatial.R
@@ -1,6 +1,24 @@
-
-
-
+#' Definition of joint non-spatial uncertainty model
+#'
+#' Function to define joint uncertainty of cross-correlated spatial objects to do Monte Carlo.
+#'
+#' @param uncert.objects Objects created by defnumarspatial
+#' @param ...
+#'
+#' @usage
+#' @return Joint uncertainty model of cross-correlated spatial objects
+#' @author Kasia Sawicka, Gerard Heuvelink
+#' @examples
+#'
+#' myjointnorm <- defjointnonspatial(uncertain = TRUE,
+#'                                  means = c(5, 8, 10),
+#'                                  varcov = matrix(c(1, 1.6, 0.2, 1.6, 4,
+#'                                                    -2.4, 0.2, -2.4, 8),
+#'                                                   nrow = 3, ncol = 3))
+#' myjointnorm
+#'
+#' @export
+#'
 defjointnonspatial <- function(uncertain, means, varcov) {
 
   jm <- list(uncertain = uncertain,
@@ -12,10 +30,3 @@ defjointnonspatial <- function(uncertain, means, varcov) {
 
 }
 
-## Example
-# myjointnorm <- defjointnonspatial(uncertain = TRUE,
-#                                   means = c(5, 8, 10),
-#                                   varcov = matrix(c(1, 1.6, 0.2, 1.6, 4,
-#                                                     -2.4, 0.2, -2.4, 8),
-#                                                     nrow = 3, ncol = 3))
-# myjointnorm
