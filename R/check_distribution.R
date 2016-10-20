366c1b755db62fbdc6eb6ace0f8b04aca5010b44
ksawicka <kasia.sawicka@wur.nl>
2016-10-20 15:51:53 +0100

    Initial. Function to test if the 'distribution' argument in defineUM() is on the list of supported distributions.

---
+++ R/check_distribution.R
@@ -0,0 +1,45 @@
+#' Simple check if distribution provided in defineUM() belongs
+#' to a list of supported distributions.
+#'
+#' @param object Any R object. In defineUM() it is used to
+#' examine if selected distribution is in supported list of ditributions.
+#'
+#' @return TRUE or FALSE.
+#'
+check_distribution <- function(object) {
+  
+  Supported_ditributions <- c("beta",        
+                              "cauchy",      
+                              "chisq",
+                              "contuni",
+                              "exp",
+                              "gamma",
+                              "logis",
+                              "lnorm",
+                              "norm",
+                              "weib",
+                              "bern",
+                              "bin",
+                              # "discrpmf",
+                              "discruni",
+                              "geom",
+                              "poiss")
+  
+  object %in% Supported_distributions
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
