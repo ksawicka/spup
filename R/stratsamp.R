5ed78f39a893698cc98194827479536985513bba
ksawicka <kasia.sawicka@wur.nl>
2016-11-07 10:25:52 +0000

    Moved code to a separate file stratsampSpatial.R.

--- R/stratsamp.R
+++ R/stratsamp.R
@@ -37,25 +37,4 @@ stratsamp <- function(object, n, p) {
   return(outmat)
 }
 
-stratsampSpatial <- function(mu, sigma, n, p) {
-  # Note that here p is a vector of probs
-  # n is sample size per stratum
-  # mu and sigma are parameters of normal distribution to be sampled
-  # function returns matrix with n rows and length(p)-1 columns
-  if (is.na(mu) || (is.na(sigma))) {
-    outmat <- matrix(NA, nrow = n, ncol = length(p)-1)
-  } else {
-    lims <- qnorm(p, mu, sigma)
-    outmat <- matrix(data = NA, nrow = n, ncol = length(p)-1)
-    counts <- rep(0, length(lims)-1)
-    while (any(counts < n)) {
-      r <- rnorm(1, mu, sigma)
-      intvl <- findInterval(r,lims)
-      if (counts[intvl] < n) {
-        counts[intvl] <- counts[intvl] + 1
-        outmat[counts[intvl], intvl] <- r
-      }
-    }
-  }
-  return(outmat)
-}
+
