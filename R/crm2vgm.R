bedb4fbbff811b2d0324572036b6a61110e783d0
ksawicka <kasia.sawicka@wur.nl>
2016-11-07 10:18:53 +0000

    Tidied up messy code.

--- R/crm2vgm.R
+++ R/crm2vgm.R
@@ -13,33 +13,18 @@
 #'
 #' @return An object of a class "variogramModel" extending data.frame.
 #' @author Kasia Sawicka, Gerard Heuvelink
-#' @examples
-#'
-#' mycormodel <- makecrm(acf0 = 0.35, range = 20, model = "Exp")
-#' myvgm <- crm2vgm(mycormodel)
-#' myvgm
-#'
-#' @export
 #'
 crm2vgm <- function(crm, ...) {
-
-  require(gstat)
-
-  # check if crm is a SpatialCorrelogram
-  if (class(crm) != "SpatialCorrelogramModel")
-    stop("crm has to be of class SpatialCorrelogramModel")
-
   nugget <- 1 - as.numeric(crm[[1]])
   # psill <- (nugget * crm[[1]])/(1 - crm[[1]]) # This is correct, but just returns value of acf0!
   psill <- crm[[1]]
   range <- crm[[2]]
   model <- crm[[3]]
-
-  vgm <- vgm(nugget = as.numeric(nugget),
-            psill = as.numeric(psill),
-            range = as.numeric(range),
-            model = model,
-            ...)
+  vgm <- gstat::vgm(nugget = as.numeric(nugget),
+                    psill = as.numeric(psill),
+                    range = as.numeric(range),
+                    model = model,
+                    ...)
   vgm
 }
 
