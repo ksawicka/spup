3fd127c0e8aa6255043c43f81589a0477473ef43
ksawicka <kasia.sawicka@wur.nl>
2016-12-06 18:07:59 +0000

    Initial. genSample() method for MarginalCategoricalSpatial class.

---
+++ R/genSample.MarginalCategoricalSpatial.R
@@ -0,0 +1,40 @@
+#' Generating Monte Carlo sample from an uncertain object of a class 
+#' 'MarginalCategoricalSpatial'
+#'
+#' @usage genSample(UMobject, n, ...)
+#'
+#' @param UMobject uncertain object defined using defineUMs().
+#' @param n Integer. Number of Monte Carlo realizations.
+#' @param ... additional parameters
+#' 
+#' @return A Monte Carlo sample of a categorical spatial variable.
+#' 
+#' @author Kasia Sawicka
+#' 
+#' @examples
+#'
+#' # load data
+#' data(house)
+#' houseUM <- defineUMs(uncertain = TRUE, categories = c(100,200), cat_prob = houses_DF)
+#' h_sample <- genSample(houseUM, n = 10)
+#' str(h_sample)
+#'
+#' houseUM <- defineUMs(uncertain = TRUE, categories = c("yes", 1), cat_prob = houses_DF)
+#'
+#' @export
+genSample.MarginalCategoricalSpatial <- function(UMobject, n, ...) {
+
+  categories <- UMobject[[2]]
+  cat_prob <- UMobject[[3]]@data
+  in1mtx <- as.matrix(cat_prob)
+  temp_samples <- t(apply(in1mtx, MARGIN = 1, 
+                          function(x) sample(categories, 
+                                             size = n, 
+                                             replace = TRUE, 
+                                             prob = x)))
+  X_sample <- UMobject[[3]]
+  X_sample@data <- as.data.frame(temp_samples)
+  names(X_sample@data) <- paste("sim", c(1:n), sep = "")
+  X_sample
+  
+}
