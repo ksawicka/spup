b32796939f1db409700f47d1a098b26975fc5fa6
ksawicka <kasia.sawicka@wur.nl>
2016-12-06 18:27:20 +0000

    Made it general for any object and distribution (i.e. no need to distinguish between spatial and non-spatial and serves more than normla distribution now).

--- R/stratsamp.R
+++ R/stratsamp.R
@@ -1,40 +1,22 @@
-# Stefan van Dam
-# 14-01-2016
-
-stratsamp <- function(object, n, p) {
-
-  # Note that here p is a vector of probs
-  # n is sample size per stratum
-  # mu and sigma are parameters of normal distribution to be sampled
-  # function returns matrix with n rows and length(p)-1 columns
-  if (object$dist == "norm") {
-    lims <- qnorm(p, object$par[1], object$par[2])
-    outmat <- matrix(NA, n, length(p)-1)
-    counts <- rep(0, length(lims)-1)
-    while(any(counts < n)){
-      r <- rnorm(1, object$par[1], object$par[2])
-      intvl <- findInterval(r,lims)
-      if(counts[intvl] < n){
-        counts[intvl] <- counts[intvl] + 1
-        outmat[counts[intvl], intvl] <- r
-      }
+#' Stratified sampling for spatial variables
+#'
+#' @param mu mean of normal distribution to be sampled
+#' @param sigma sd of normal distribution to be sampled
+#' @param n sample size per stratum
+#' @param p vector of probs
+#'
+#' @return Sample of spatial variable. Matrix with n rows and length(p)-1 columns.
+stratsamp <- function(n, distribution, parameters, p) {
+  lims <- find_strata(p, distribution, parameters)
+  outmat <- matrix(data = NA, nrow = n, ncol = length(p)-1)
+  counts <- rep(0, length(lims)-1)
+  while (any(counts < n)) {
+    r <- distribution_sampling(1, distribution, parameters)
+    intvl <- findInterval(r, lims)
+    if (counts[intvl] < n) {
+      counts[intvl] <- counts[intvl] + 1
+      outmat[counts[intvl], intvl] <- r
     }
   }
-
-  if (object$dist == "log") {
-    lims <- qlnorm(p, object$par[1], object$par[2])
-    outmat <- matrix(NA, n, length(p)-1)
-    counts <- rep(0, length(lims)-1)
-    while(any(counts < n)){
-      r <- rlnorm(1, object$par[1], object$par[2])
-      intvl <- findInterval(r,lims)
-      if(counts[intvl] < n){
-        counts[intvl] <- counts[intvl] + 1
-        outmat[counts[intvl], intvl] <- r
-      }
-    }
-  }
-  return(outmat)
+  outmat
 }
-
-
