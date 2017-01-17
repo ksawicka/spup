2138e41fe2197c5f8babc8a30cf7c1277120dfba
ksawicka <kasia.sawicka@wur.nl>
2017-01-17 17:46:38 +0100

    Edit to examples where name 'defineUMs' changed to 'defineUM'.

--- R/quantile.SpatialGridDataFrame.R
+++ R/quantile.SpatialGridDataFrame.R
@@ -9,7 +9,7 @@
 #' 
 #' data(DEM)
 #' dem_crm <- makecrm(acf0 = 0.78, range = 321, model = "Exp")
-#' demUM <- defineUMs(uncertain = TRUE, distribution = "norm", 
+#' demUM <- defineUM(uncertain = TRUE, distribution = "norm", 
 #'                    distr_param = c(dem30m, dem30m_sd), crm = dem_crm)
 #' dem_sample <- genSample(UMobject = demUM, n = 50, samplemethod = "ugs", nmax = 20)
 #' slope_sample <- propagate1(realizations = dem_sample, model = Slope, n = 50)
