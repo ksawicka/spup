48603ceade591d15f6c900236ab2972e6a1ec663
ksawicka <kasia.sawicka@wur.nl>
2017-01-18 11:47:34 +0100

    Initial. Function to find depth of a list.

---
+++ R/depth.R
@@ -0,0 +1,17 @@
+#' Title depth
+#'
+#' @param List an object of class 'list'
+#'
+#' @return an integer; level of list nesting
+#'
+#' @examples
+#' 
+#' a <- list(1,2)
+#' depth(a)
+#' 
+#' a <- list(list(1, 2), 3)
+#' depth(a)
+#' 
+depth <- function(List) {
+  ifelse(is.list(List), 1L + max(sapply(List, depth)), 0L)
+}
