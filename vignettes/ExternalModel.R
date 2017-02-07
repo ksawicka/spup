a12b7103a1347bd29a92886f0a75c4ee11869dca
ksawicka <kasia.sawicka@wur.nl>
2017-02-07 17:31:38 +0100

    Initial. Started file of the vignette on uncertainty analysis with external model.

---
+++ vignettes/ExternalModel.R
@@ -0,0 +1,20 @@
+## ---- echo = FALSE-------------------------------------------------------
+knitr::opts_chunk$set(
+    comment = NA,
+    quiet = TRUE,
+    progress = FALSE,
+    tidy = FALSE,
+    cache = FALSE,
+    message = FALSE,
+    error = FALSE, # FALSE: always stop execution.
+    warning = TRUE,
+    dpi = 100
+)
+
+## ---- echo = FALSE-------------------------------------------------------
+knitr::opts_knit$set(global.par = TRUE)
+
+## ---- echo = FALSE-------------------------------------------------------
+par(mar = c(3, 3, 2, 2), mgp = c(1.7, 0.5, 0), las = 1, cex.main = 1, tcl = -0.2, cex.axis = 0.8,
+    cex.lab = 0.8)
+
