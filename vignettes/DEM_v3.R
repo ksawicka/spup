7d8399a1b73617e30b1c5ea4e96ab857a037d1db
ksawicka <kasia.sawicka@wur.nl>
2017-02-10 15:44:01 +0100

    Update.

--- vignettes/DEM_v3.R
+++ vignettes/DEM_v3.R
@@ -18,17 +18,18 @@ knitr::opts_knit$set(global.par = TRUE)
 par(mar = c(3, 3, 2, 2), mgp = c(1.7, 0.5, 0), las = 1, cex.main = 1, tcl = -0.2, cex.axis = 0.8,
     cex.lab = 0.8)
 
-## ---- fig.width = 5, fig.height = 3--------------------------------------
+## ---- fig.width = 5, fig.height = 6--------------------------------------
 # load packages
 library(sp)
 library(spup)
+library(gridExtra)
 
 # load and view the data
 data(DEM)
 str(dem30m)
 str(dem30m_sd)
-spplot(dem30m, main = list(label = "Mean DEM", cex = 1))
-summary(dem30m_sd)
+grid.arrange(spplot(dem30m, main = list(label = "Mean DEM [m]", cex = 1)),
+             spplot(dem30m_sd, main = list(label = "DEM sd [m]", cex = 1)))
 
 ## ------------------------------------------------------------------------
 # define spatial correlogram model
@@ -59,28 +60,22 @@ class(demUM)
 dem_sample <- genSample(UMobject = demUM, n = 100, samplemethod = "ugs", nmax = 20, asList = FALSE)
 
 # view several realizations of DEM
-spplot(dem_sample[c(5,6,3,4,1,2)], main = list(label = "Examples of the slope realizations", cex = 1))
+spplot(dem_sample[c(5,6,3,4,1,2)], main = list(label = "Examples of the DEM realizations", cex = 1))
 
 ## ---- fig.width = 5, fig.height = 6--------------------------------------
 # compute and plot the slope sample statistics
 # e.g. mean and standard deviation
 dem_sample_mean <- mean(dem_sample)
 dem_sample_sd <- sd(dem_sample)
-m <- spplot(dem_sample_mean, main = list(label = "Mean of the DEM realizations", cex = 1))
-s <- spplot(dem_sample_sd, main = list(label = "Standard dev. of the DEM realizations", cex = 1))
-print(m, split = c(1, 1, 1, 2), more = TRUE)
-print(s, split = c(1, 2, 1, 2))
-rm(m, s)
+grid.arrange(spplot(dem_sample_mean, main = list(label = "Mean of the DEM realizations", cex = 1)),
+             spplot(dem_sample_sd, main = list(label = "Standard dev. of the DEM realizations", cex = 1)))
 
 ## ---- fig.width = 5, fig.height = 6--------------------------------------
 dem_crm2 <- makecrm(acf0 = 0.2, range = 300, model = "Exp")
 demUM2 <- defineUM(uncertain = TRUE, distribution = "norm", distr_param = c(dem30m, dem30m_sd), crm = dem_crm2)
 dem_sample2 <- genSample(UMobject = demUM2, n = 100, samplemethod = "ugs", nmax = 20, asList = FALSE)
-s1 <- spplot(dem_sample, c(1), main = list(label = "dem_sample, acf0 = 0.8, range = 300m", cex = 1))
-s2 <- spplot(dem_sample2, c(1), main = list(label = "dem_sample2, acf0 = 0.2, range = 300m", cex = 1))
-print(s1, split = c(1, 1, 1, 2), more = TRUE)
-print(s2, split = c(1, 2, 1, 2))
-rm(s1, s2)
+grid.arrange(spplot(dem_sample, c(1), main = list(label = "dem_sample, acf0 = 0.8, range = 300m", cex = 1)),
+             spplot(dem_sample2, c(1), main = list(label = "dem_sample2, acf0 = 0.2, range = 300m", cex = 1)))
 
 ## ------------------------------------------------------------------------
 # view the model
@@ -115,38 +110,38 @@ spplot(slope_sample[c(5,6,3,4,1,2)], main = list(label = "Examples of the slope 
 # e.g. mean and standard deviation
 slope_mean <- mean(slope_sample)
 slope_sd <- sd(slope_sample, na.rm = TRUE) # na.rm = TRUE is necessary, because slope cannot be calculated at the border
-m <- spplot(slope_mean, main = list(label = "Mean of the slope realizations", cex = 1))
-s <- spplot(slope_sd, main = list(label = "Standard dev. of the slope realizations", cex = 1))
-print(m, split = c(1, 1, 1, 2), more = TRUE)
-print(s, split = c(1, 2, 1, 2))
-rm(m, s)
+grid.arrange(spplot(slope_mean, main = list(label = "Mean of the slope realizations", cex = 1)),
+             spplot(slope_sd, main = list(label = "Standard dev. of the slope realizations", cex = 1)))
+
+## ---- fig.width = 5, fig.height = 3--------------------------------------
+# selec a couple of locations and plot points on the slope map
+loc1 <- 2200
+loc2 <- 6200
+points <- data.frame(t(coordinates(slope_sample)[loc1,]))
+points[2,] <- t(coordinates(slope_sample)[loc2,])
+coordinates(points) <- ~ s1 + s2
+proj4string(points) <- proj4string(slope_sample)
+spplot(slope_sample, c(1), col.regions = grey.colors(16),
+       sp.layout = c('sp.points', points, col = "red", pch = 19, cex = 1.2))
 
 ## ---- fig.width = 7, fig.height = 3--------------------------------------
-# check the histogram of slope at lowest and highest DEM 
-l <- which(dem30m@data == min(dem30m@data)) # there is no slope calculated for this outer value so we need to fins a different one
-l <- which(dem30m@data < 860)
-l
-# lets take location 400 for l.
-h <- which(dem30m@data == max(dem30m@data))
-h
-l_mean <- mean(as.numeric(slope_sample@data[400,]))
-l_sd <- sd(as.numeric(slope_sample@data[400,]))
-h_mean <- mean(as.numeric(slope_sample@data[6898,]))
-h_sd <- sd(as.numeric(slope_sample@data[6898,]))
+l_mean <- mean(as.numeric(slope_sample@data[loc1,]))
+l_sd <- sd(as.numeric(slope_sample@data[loc1,]))
+h_mean <- mean(as.numeric(slope_sample@data[loc2,]))
+h_sd <- sd(as.numeric(slope_sample@data[loc2,]))
 
 par(mfrow = c(1,2))
-hist(as.numeric(slope_sample@data[400,]), main = paste("Slope at low DEM,", "\n",
+hist(as.numeric(slope_sample@data[loc1,]), main = paste("Slope at high DEM sd,", "\n",
      "mean = ", round(l_mean, 2), ", sd = ", round(l_sd, 2), sep = ""), xlab = "Slope")
-hist(as.numeric(slope_sample@data[6898,]), main = paste("Slope at high DEM,", "\n",
+hist(as.numeric(slope_sample@data[loc2,]), main = paste("Slope at low DEM sd,", "\n",
      "mean = ", round(h_mean, 2), ", sd = ", round(h_sd, 2), sep = ""), xlab = "Slope")
-rm(l, h)
 
 ## ---- fig.width = 5, fig.height = 5--------------------------------------
 # scatter plot of slope against elevation
 library(GGally)
 slope1 <- Slope(dem30m)
 names(slope1@grid@ cellcentre.offset) <- c("x", "y")
-df <- cbind(dem30m@data, slope1@data)
+df <- cbind(dem30m_sd@data, slope1@data)
 ggscatmat(data = df, alpha=0.15)
 
 ## ---- fig.width = 7, fig.height = 5--------------------------------------
