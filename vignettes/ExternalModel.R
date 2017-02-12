08dbd9658058647022203423aa1ed222b9540e05
ksawicka <kasia.sawicka@wur.nl>
2017-02-12 16:27:19 +0100

    Update.

--- vignettes/ExternalModel.R
+++ vignettes/ExternalModel.R
@@ -18,3 +18,45 @@ knitr::opts_knit$set(global.par = TRUE)
 par(mar = c(3, 3, 2, 2), mgp = c(1.7, 0.5, 0), las = 1, cex.main = 1, tcl = -0.2, cex.axis = 0.8,
     cex.lab = 0.8)
 
+## ------------------------------------------------------------------------
+# library(magrittr) # piping
+# library(whisker)  # templating
+# library(readr)    # fast I/O (huge files)
+# library(purrr)    # functional programming tools
+# library(dplyr)    # a grammar of data manipulation
+
+## ------------------------------------------------------------------------
+template <- function(filenames) {
+    filenames %>% 
+        walk(
+            function(x) {
+                if (!grepl(pattern = "\\.template$", x = x)) {
+                    stop(
+                        "File %s does not have extension 'template'" %>% 
+                            sprintf(x %>% sQuote), 
+                        call. = FALSE
+                    )
+                }
+            }
+        )
+    filenames %>% 
+        walk(
+            function(x) {
+                if (!file.exists(x)) {
+                    stop(
+                        "File %s not found" %>% 
+                            sprintf(x %>% sQuote), 
+                        call. = FALSE
+                    )
+                }
+            }
+        )
+    class(filenames) <- "template"
+    filenames
+}
+
+print.template <- function(x, ...) {
+    cat("container containing the following template(s):\n")
+    print(as.character(x))
+}
+
