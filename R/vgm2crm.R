8ecacc5d50e621afd13bec9abd910607ceb2fb9d
Kasia <kasia.sawicka@ed.ac.uk>
2018-06-27 11:53:28 +0100

    add more checks and working arguments

--- R/vgm2crm.R
+++ R/vgm2crm.R
@@ -6,42 +6,41 @@
 #' @param range See ?vgm
 #' @param model See ?vgm
 #' @param kappa See ?vgm
-#' @param anis See ?vgm
 #' @param Err See ?vgm
-#' 
 #'
 #' @return Spatial correlogram model - standardised parameters of spatial variogram model
 #' 
 #' @author Kasia Sawicka
 #' 
 #' 
-vgm2crm <- function(vgm, psill, nugget, range, model, kappa = 0.5, anis, Err = 0) {
+vgm2crm <- function(vgm, psill, nugget, range, model, kappa = 0.5, Err = 0) {
 
   if (!missing(vgm)) {
+    
     if (length(vgm$psill) == 1) {
       psill <- vgm$psill[1]
+      nugget <- 0
       range <- vgm$range[1]
-	  model <- vgm$model[1]
-	  nugget <- 0
+      model <- vgm$model[1]
     } else {
-	  psill <- vgm$psill[2]
+      psill <- vgm$psill[2]
+      nugget <- vgm$psill[1]
       range <- vgm$range[2]
-	  model <- vgm$model[2]
-	  nugget <- vgm$psill[1]
-	}
+      model <- vgm$model[2]
+    }
     anis <- vgm$anis
     kappa <- vgm$kappa
     Err <- vgm$Err
+  } else {
+  
+    stopifnot(length(psill) == 1) 
+    stopifnot(length(range) == 1)
+    stopifnot(length(model) == 1)
+    
+    if (missing(nugget)) nugget <- 0
+    if (missing(Err)) Err <- NULL
   }
-  
-  stopifnot(length(psill) == 1) 
-  stopifnot(length(range) == 1)
-  stopifnot(length(model) == 1)
-  
-  if (missing(nugget)) nugget <- 0
-  if (missing(anis)) anis <- NULL
-  
-  
+
   acf0 <- psill/(psill + nugget)
   crm <- list(acf0 = acf0,
              range = range,
@@ -54,6 +53,3 @@ vgm2crm <- function(vgm, psill, nugget, range, model, kappa = 0.5, anis, Err = 0
   crm
 }
 
-a=vgm(10, "Exp", 300, nugget = 2)
-b=vgm2crm(vgm=a)
-b=vgm2crm(psill = 8, range = 100, model = "Exp")
